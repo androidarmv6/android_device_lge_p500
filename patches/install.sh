67caaaf06e31100b03aed39e658d4799d478c5e7
GenetICS <gntcs@msn.com>
2012-12-01 05:40:29 +0200

    Patches

---
+++ patches/install.sh
@@ -0,0 +1,38 @@
+echo $1
+rootdirectory="$PWD"
+# ---------------------------------
+
+
+cd $rootdirectory
+cd bionic
+echo "Applying bionic patches..."
+git apply $rootdirectory/device/lge/p500/patches/bionic/*.patch
+echo " "
+
+cd $rootdirectory
+cd external/webkit/
+echo "Applying external/webkit/ patches..."
+git apply $rootdirectory/device/lge/p500/patches/external/webkit/*.patch
+echo " "
+
+cd $rootdirectory
+cd packages/apps/Apollo/
+echo "Applying packages/apps/Apollo/ patches..."
+git apply $rootdirectory/device/lge/p500/patches/packages/apps/Apollo/*.patch
+echo " "
+
+cd $rootdirectory
+cd packages/apps/Mms/
+echo "Applying packages/apps/Mms/ patches..."
+git apply $rootdirectory/device/lge/p500/patches/packages/apps/Mms/*.patch
+echo " "
+
+cd $rootdirectory
+cd system/su/
+echo "Applying system/su/ patches..."
+git apply $rootdirectory/device/lge/p500/patches/system/su/*.patch
+echo " "
+
+# -----------------------------------
+echo "Changing to build directory..."
+cd $rootdirectory
