b37fc4ce1dc5f2087e2cdef73d767c6958cf3e3b
Lalit Maganti <lalitmaganti@gmail.com>
2012-12-22 22:58:41 +0000

    p500: cleanups

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,11 +8,3 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p500/bluetooth
 
 TARGET_BOOTLOADER_BOARD_NAME := p500
 TARGET_OTA_ASSERT_DEVICE := thunderg,p500
-
-USE_CAMERA_STUB := false
-
-TARGET_PROVIDES_LIBAUDIO := true
-# Enable ICS-backwards compatibility    
-COMMON_GLOBAL_CFLAGS += -DQCOM_ICS_COMPAT
-# Disable PIE since it breaks ICS camera blobs
-TARGET_DISABLE_ARM_PIE := true
