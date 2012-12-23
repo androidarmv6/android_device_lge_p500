09e719548acdf3fdca81323efadb0d221b03e793
GenetICS <gntcs@msn.com>
2012-12-23 22:22:21 +0200

    Add android_vendor_lge_msm7x27-common repo

--- p500.mk
+++ p500.mk
@@ -2,7 +2,8 @@ PRODUCT_AAPT_CONFIG := normal mdpi
 PRODUCT_AAPT_PREF_CONFIG := mdpi
 
 $(call inherit-product, device/lge/msm7x27-common/device.mk)
-$(call inherit-product-if-exists, vendor/lge/p500/p500-vendor.mk)
+$(call inherit-product, vendor/lge/p500/p500-vendor.mk)
+$(call inherit-product, vendor/lge/msm7x27-common/msm7x27-common-vendor-blobs.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/p500/overlay
 
