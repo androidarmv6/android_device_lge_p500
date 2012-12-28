825f7f13373b913bed230e0f5c14eb047fad80ad
AndroidMeda <hephappy@gmail.com>
2012-12-28 19:57:29 +0200

    Fix bluetooth 1/2 (WIP)
    Change-Id: I640393be84528e95451a132ec3a54b56b8d6b3d0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,7 +4,5 @@
 TARGET_KERNEL_CONFIG := cyanogenmod_p500_p509_defconfig
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
 
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p500/bluetooth
-
 TARGET_BOOTLOADER_BOARD_NAME := p500
 TARGET_OTA_ASSERT_DEVICE := thunderg,p500
