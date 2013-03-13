a320259945aee1b089cc1a0b10ca86d69b1bc686
AndroidMeda <hephappy@gmail.com>
2013-03-13 07:20:00 +0200

    BCM4325: FM library for new proposal fm api
    Change-Id: I9c2c11f15da54c65291a7e65206180e4f52c0d4a

--- device.mk
+++ device.mk
@@ -15,7 +15,8 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/base/data/etc/com.stericsson.hardware.fm.receiver.xml:system/etc/permissions/com.stericsson.hardware.fm.receiver.xml
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/CHANGELOG-CM.txt:system/etc/CHANGELOG-CM.txt
@@ -127,7 +128,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     HwaSettings
 
-# chargermode
+PRODUCT_PACKAGES += \
+    FmRadio \
+    FmRadioReceiver \
+    libfmradio.bcm4325
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/chargemode/chargerimages/battery_ani_01.rle:root/chargerimages/battery_ani_01.rle \
     $(LOCAL_PATH)/chargemode/chargerimages/battery_ani_02.rle:root/chargerimages/battery_ani_02.rle \
