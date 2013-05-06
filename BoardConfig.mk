cde8905bfda9fca8733e888df4c03a994a8794a3
AndroidMeda <hephappy@gmail.com>
2013-05-06 18:58:17 +0300

    Get rid of novatek (1/2)
    Change-Id: I1d1d265c9d6491dd023056a1d153e024751b7e03

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,7 @@
 -include vendor/lge/p500/BoardConfigVendor.mk
 -include device/lge/msm7x27-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := cyanogenmod_p500_p509_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_p500_defconfig
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
 COMMON_GLOBAL_CFLAGS += -DBOARD_CHARGING_CMDLINE_NAME='"lge.reboot"' -DBOARD_CHARGING_CMDLINE_VALUE='"pwroff"'
 
