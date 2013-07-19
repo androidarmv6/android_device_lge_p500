f8613f6430eabad02a95bbf74fc685d1b2a7481c
AndroidMeda <hephappy@gmail.com>
2013-07-19 13:36:57 +0300

    p500: Enable selinux
    Change-Id: I6ed5f828a4f7d5425500c52eb6fd051be6746808

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,8 @@
 -include vendor/lge/p500/BoardConfigVendor.mk
 -include device/lge/msm7x27-common/BoardConfigCommon.mk
 
+HAVE_SELINUX := true
+
 TARGET_KERNEL_CONFIG := cyanogenmod_p500_defconfig
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
 COMMON_GLOBAL_CFLAGS += -DBOARD_CHARGING_CMDLINE_NAME='"lge.reboot"' -DBOARD_CHARGING_CMDLINE_VALUE='"pwroff"'
