1d82d9a3102773be622cd5d5f37b9172a1bf0109
AndroidMeda <hephappy@gmail.com>
2013-11-24 23:11:29 +0100

    Remove charger binaries to fit recovery in eng build
    We don't need charger in recovery mod,
    so charger binaries is kept in boot.img to run offmode charging.
    
    Change-Id: I91f388724ccefe2a60eb21ecfa424f338d6c4d7c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,8 +3,12 @@
 
 TARGET_KERNEL_CONFIG := cyanogenmod_p500_defconfig
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
+ifneq (eng,$(TARGET_BUILD_VARIANT))
 COMMON_GLOBAL_CFLAGS += -DBOARD_CHARGING_CMDLINE_NAME='"lge.reboot"' -DBOARD_CHARGING_CMDLINE_VALUE='"pwroff"'
-
+else
+# Real recovery size
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+endif
 TARGET_BOOTLOADER_BOARD_NAME := p500
 TARGET_OTA_ASSERT_DEVICE := thunderg,p500
 TARGET_RECOVERY_FSTAB := device/lge/p500/fstab.p500
@@ -12,4 +16,3 @@ TARGET_RECOVERY_FSTAB := device/lge/p500/fstab.p500
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p500/bluetooth
 
 P500_SPEAKER_IN_CALL_FIX := true
-
