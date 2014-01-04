b1e4a18ead321d1015899b1038a4a730897f3536
androidmeda <hephappy@gmail.com>
2014-01-04 18:07:47 +0200

    Build libcamera from source (3/3)
    Change-Id: I44300a37c70668f2991c1cf33e1bed57d2a16ce5

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -30,7 +30,6 @@ OUTDIR=vendor/$VENDOR/$DEVICE
 PRODUCT_COPY_FILES += \\
     $OUTDIR/proprietary/lib/libmmipl.so:obj/lib/libmmipl.so \\
     $OUTDIR/proprietary/lib/libmmjpeg.so:obj/lib/libmmjpeg.so \\
-    $OUTDIR/proprietary/lib/libcamera.so:obj/lib/libcamera.so \\
     $OUTDIR/proprietary/lib/liboemcamera.so:obj/lib/liboemcamera.so
 
 
