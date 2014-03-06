e3e1ca936754f38ee378554235c2dfde9471c89f
androidmeda <hephappy@gmail.com>
2014-03-06 14:56:46 +0200

    Recovery config for p500
    Remove some debug, ipv6 and netfilter options
    Reduced 200KB
    
    Change-Id: I09a60a70077ac5013fdbcc26769349c5e7c294c4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,13 +1,15 @@
 -include vendor/lge/p500/BoardConfigVendor.mk
 -include device/lge/msm7x27-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := cyanogenmod_p500_defconfig
+
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
 ifneq (eng,$(TARGET_BUILD_VARIANT))
+TARGET_KERNEL_CONFIG := cyanogenmod_p500_defconfig
 COMMON_GLOBAL_CFLAGS += -DBOARD_CHARGING_CMDLINE_NAME='"lge.reboot"' -DBOARD_CHARGING_CMDLINE_VALUE='"pwroff"'
 else
 # Real recovery size
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+TARGET_KERNEL_CONFIG := cyanogenmod_p500_recovery_defconfig
 endif
 TARGET_BOOTLOADER_BOARD_NAME := p500
 TARGET_OTA_ASSERT_DEVICE := thunderg,p500
