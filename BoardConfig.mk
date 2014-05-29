1e8886e02c706c058dfa73d4415e5af1216baf6e
androidmeda <hephappy@gmail.com>
2014-05-29 14:16:56 +0300

    Update recovery options (2/2)
    - After lzma compression of ramdisk, we have enough space
    
    Change-Id: Ib78aedd7a2bcbf7f0ceb755b5b7e1fa998b4e12a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,20 +1,14 @@
 -include vendor/lge/p500/BoardConfigVendor.mk
 -include device/lge/msm7x27-common/BoardConfigCommon.mk
 
-
+BOARD_CHARGING_CMDLINE_NAME := "lge.reboot"
+BOARD_CHARGING_CMDLINE_VALUE := "pwroff"
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
-ifneq (eng,$(TARGET_BUILD_VARIANT))
-TARGET_KERNEL_CONFIG := cyanogenmod_p500_defconfig
-COMMON_GLOBAL_CFLAGS += -DBOARD_CHARGING_CMDLINE_NAME='"lge.reboot"' -DBOARD_CHARGING_CMDLINE_VALUE='"pwroff"'
-else
-# Real recovery size
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
-TARGET_KERNEL_CONFIG := cyanogenmod_p500_recovery_defconfig
-endif
+TARGET_KERNEL_CONFIG := cyanogenmod_p500_defconfig
 TARGET_BOOTLOADER_BOARD_NAME := p500
 TARGET_OTA_ASSERT_DEVICE := thunderg,p500
 TARGET_RECOVERY_FSTAB := device/lge/p500/fstab.p500
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p500/bluetooth
 
-P500_SPEAKER_IN_CALL_FIX := true
--- p500.mk
+++ p500.mk
@@ -19,8 +19,6 @@ PRODUCT_COPY_FILES += \
 #PRODUCT_COPY_FILES += \
 #    frameworks/base/data/etc/com.stericsson.hardware.fm.receiver.xml:system/etc/permissions/com.stericsson.hardware.fm.receiver.xml
 
-# p500 off-mode charging (only userdebug build)
-ifneq (eng,$(TARGET_BUILD_VARIANT))
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/checkbootreason:root/sbin/checkbootreason
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/chargemode/chargerimages/battery_ani_01.rle:root/chargerimages/battery_ani_01.rle \
@@ -39,7 +37,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/chargemode/chargerimages/battery_wait_ani_02.rle:root/chargerimages/battery_wait_ani_02.rle \
     $(LOCAL_PATH)/chargemode/chargerimages/black_bg.rle:root/chargerimages/black_bg.rle \
     $(LOCAL_PATH)/chargemode/chargerlogo:root/sbin/chargerlogo
-endif
 
 # P500 bluetooth vendor configuration
 PRODUCT_COPY_FILES += \
