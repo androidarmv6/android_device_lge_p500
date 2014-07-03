ac4ef9a926051dc06c730ce428927126766a4c5d
androidmeda <hephappy@gmail.com>
2014-07-03 13:45:04 +0300

    Use lzma for only recovery ramdisk (2/2)
    Change-Id: I9d0ace9b1fbb40eab1af00d54fa24064540670ca

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,21 +1,21 @@
 -include vendor/lge/p500/BoardConfigVendor.mk
 -include device/lge/msm7x27-common/BoardConfigCommon.mk
 
-BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+TARGET_BOOTLOADER_BOARD_NAME    := p500
+TARGET_OTA_ASSERT_DEVICE        := thunderg,p500
+TARGET_RECOVERY_FSTAB           := device/lge/p500/fstab.p500
+BOARD_KERNEL_CMDLINE            := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
 
-# Don't include charger detection in recovery mode
 ifneq (eng,$(TARGET_BUILD_VARIANT))
-TARGET_KERNEL_CONFIG := cyanogenmod_p500_defconfig
-BOARD_CHARGING_CMDLINE_NAME := "lge.reboot"
-BOARD_CHARGING_CMDLINE_VALUE := "pwroff"
+# Don't include charger detection in recovery mode
+TARGET_KERNEL_CONFIG                := cyanogenmod_p500_defconfig
+BOARD_CHARGING_CMDLINE_NAME         := "lge.reboot"
+BOARD_CHARGING_CMDLINE_VALUE        := "pwroff"
 else
-TARGET_KERNEL_CONFIG := cyanogenmod_p500_recovery_defconfig
+# Real recovery size 0x00500000. Use lzma.
+TARGET_KERNEL_CONFIG                := cyanogenmod_p500_recovery_defconfig
+BOARD_RECOVERYIMAGE_PARTITION_SIZE  := 5242880
+MINIGZIP                            := $(shell which lzma)
 endif
 
-TARGET_BOOTLOADER_BOARD_NAME := p500
-TARGET_OTA_ASSERT_DEVICE := thunderg,p500
-TARGET_RECOVERY_FSTAB := device/lge/p500/fstab.p500
-
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p500/bluetooth
-
