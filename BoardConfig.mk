32779321984e4184cd607334b02dd5eae34738ef
Conn O'Griofa <connogriofa@gmail.com>
2014-07-22 03:00:48 +0100

    fstab.p500: don't hardcode fs type, don't install as recovery.fstab
    Use separate recovery.fstab in msm7x27-common for recovery to
    fix sdcard partitioning.
    
    Change-Id: I2aaa1eb29f42c9a3be467a586f7be1060db24a2a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,6 @@
 
 TARGET_BOOTLOADER_BOARD_NAME    := p500
 TARGET_OTA_ASSERT_DEVICE        := thunderg,p500
-TARGET_RECOVERY_FSTAB           := device/lge/p500/fstab.p500
 BOARD_KERNEL_CMDLINE            := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
 
 ifneq (eng,$(TARGET_BUILD_VARIANT))
